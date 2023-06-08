0f57085efd5e8d667ac097ac9c86d7a5ae5b0188
ConstantineVac <108877593+ConstantineVac@users.noreply.github.com>
2023-06-08 16:56:25 +0300

    Update weather.py

--- WeatherApp/weather.py
+++ WeatherApp/weather.py
@@ -1,10 +1,10 @@
 import requests
 
 url = "http://api.openweathermap.org/data/2.5/weather"
-api_key = "93af1d7123fdf9a069485b6bcb6c39f5"
+api_key = "your_api_key"
 
 params = {
-    "q": "Athens",  # Replace "YourCityName" with the actual city name
+    "q": "your_city",  # Replace "YourCityName" with the actual city name
     "appid": api_key,
     "units": "metric"  # You can change the units to "imperial" for Fahrenheit
 }
